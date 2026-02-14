42575fefaf05d1b57de3890b220679524f3c7234
unknown <rodolfocfelis@gmail.com>
2026-02-14 16:32:37 -0300

    edicao do frontend para rodar pelo render

--- inventory-frontend/src/api.js
+++ inventory-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080'
-});
+const API_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:8080' 
+    : 'https://sua-api-no-render.onrender.com';
 
-export default api;
+export default API_URL;
