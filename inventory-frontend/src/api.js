d9188a3db8723d14e3cf939c2b0b1451f8deb1c0
unknown <rodolfocfelis@gmail.com>
2026-02-14 17:48:14 -0300

    fix: edit no api.js para rodar o front no render

--- inventory-frontend/src/api.js
+++ inventory-frontend/src/api.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const API_URL = window.location.hostname === 'localhost' 
     ? 'http://localhost:8080' 
-    : 'https://sua-api-no-render.onrender.com';
+    : 'https://autoflex-inventory-system.onrender.com';
 
-export default API_URL;
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export default api;
